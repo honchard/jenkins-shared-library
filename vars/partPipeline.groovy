b9f918ac72fb52d328b67392cf700b2b8f030f2e
gonchard@gmail.com <honchard>
2022-03-29 00:26:52 +0400

    partPipeline class

---
+++ vars/partPipeline.groovy
@@ -0,0 +1,32 @@
+def call(body) {
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
+  stage('Checkout') {
+    steps {
+      //checkout scm
+      echo "checkout"
+    }
+  }
+  stage('Get Dockerfile') {
+    steps {
+      script {
+        def tmpFile = libraryResource 'Dockerfile'
+        writeFile file: 'Dockerfile', text: tmpFile
+      }
+    }
+  }
+  stage('Build') {
+    steps {
+      script {
+        sh '''
+          echo "version"
+          echo "build -t ealebed/hellonode:latest ."
+          echo "image ls"
+        '''
+      }
+    }
+  }
+}
